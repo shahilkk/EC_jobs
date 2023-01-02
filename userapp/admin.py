d58bf68df2542a742fd46d3a301cb2ea534abed5
shahilkk <shahilkhanarimbra@gmail.com>
2023-01-02 21:55:24 +0530

    first

---
+++ userapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from . models import *
+# Register your models here.
+
+
+admin.site.register(Login)
+admin.site.register(UserDetails)
+admin.site.register(WorkerDetails)
+admin.site.register(Quick_booking)
+admin.site.register(Booking)
+admin.site.register(Package)
+admin.site.register(Contact)
